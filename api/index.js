bd38a5e8ef18c7acc42aa35d24c21ece97560ad2
callum <callumjosephiaingordon@gmail.com>
2022-09-19 16:03:49 +0200

    installed swagger ui

--- api/index.js
+++ api/index.js
@@ -11,8 +11,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const swaggerDoc = require('./swagger.json');
+
 //ROUTER:
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto' , config.api.port);
