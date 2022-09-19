f70a3ffde58a50894d563b4e7807def8d23fae84
callum <callumjosephiaingordon@gmail.com>
2022-09-19 17:30:29 +0200

    created network.js in auth folder to manage the login function

--- store/dummy.js
+++ store/dummy.js
@@ -1,16 +1,16 @@
 const db = {
     'user': [
-        { id: '1', name: 'Callum' },
+        { id: '1', name: 'Carlos' },
     ],
 };
 
 async function list(tabla) {
-    return db [tabla];
+    return db[tabla] || [];
 }
 
 async function get(tabla, id) {
     let col = await list(tabla);
-    return col.filter(item => item.id === id) [0] || null; 
+    return col.filter(item => item.id === id)[0] || null;
 }
 
 async function upsert(tabla, data) {
@@ -27,10 +27,18 @@ async function remove(tabla, id) {
     return true;
 }
 
+async function query(tabla, q) {
+    let col = await list(tabla);
+    let keys = Object.keys(q);
+    let key = keys[0];
+    
+    return col.filter(item => item[key] === q[key])[0] || null;
+}
 
 module.exports = {
     list,
     get,
     upsert,
     remove,
+    query,
 };
