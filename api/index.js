b4bc3c0de81efebce2a9e32c13ea446f23b8dcd3
callum <callumjosephiaingordon@gmail.com>
2022-09-19 17:46:12 +0200

    created auth folder and corresponding index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config');
 const user = require('./components/user/network');
+const auth = require('./components/auth/network');
 
 const app = express(); 
 
@@ -15,6 +16,7 @@ const swaggerDoc = require('./swagger.json');
 
 //ROUTER:
 app.use('/api/user', user);
+app.use('/api/auth', auth);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
